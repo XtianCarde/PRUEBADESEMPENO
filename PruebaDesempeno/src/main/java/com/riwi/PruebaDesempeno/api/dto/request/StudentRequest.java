0667ee4ead6f54e50828ed2dba0ed2b078fca08e
Cristian <tiancarvelasquez@gmail.com>
2024-06-05 08:04:52 -0500

    Add validation email

--- PruebaDesempeno/src/main/java/com/riwi/PruebaDesempeno/api/dto/request/StudentRequest.java
+++ PruebaDesempeno/src/main/java/com/riwi/PruebaDesempeno/api/dto/request/StudentRequest.java
@@ -1,5 +1,6 @@
 package com.riwi.PruebaDesempeno.api.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -18,7 +19,12 @@ public class StudentRequest {
     @NotBlank(message = "El nombre es requerido")
     @Size(min = 4, max = 50, message = "El nombre debe contener entre 4 y 50 caracteres")
     private String name;
-    @NotBlank(message = "El email es requerido")
+    @Size(
+        min = 1, 
+        max = 100, 
+        message = "El email debe tener entre 1 y 100 caracteres"
+    )
+    @Email(message = "Ingresa un email valido")
     private String email;
     @NotNull(message = "El id de la clase es requerido")
     @Min(value = 1, message = "El id debe ser mayor que 0")
