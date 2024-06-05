c9739731cfe88d87ce3e0ca61d3a268fca04f3c1
Cristian <tiancarvelasquez@gmail.com>
2024-06-05 09:00:43 -0500

    Add dto response ClassOfStudent

--- PruebaDesempeno/src/main/java/com/riwi/PruebaDesempeno/api/dto/response/ClassOfStudent.java
+++ PruebaDesempeno/src/main/java/com/riwi/PruebaDesempeno/api/dto/response/ClassOfStudent.java
@@ -1,19 +1,16 @@
 package com.riwi.PruebaDesempeno.api.dto.response;
 
-import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
-public class StudentBasicResp {
-    private Long id;
-    private String name;
-    private String email;
-    private LocalDateTime createdAt;
-    private Boolean isActive;
+public class ClassOfStudent extends StudentBasicResp {
+    ClassBasicResp classBasicResp;
 }
