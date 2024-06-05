f5180cec99fea003c7b5b880d77e209fd3257baa
Cristian <tiancarvelasquez@gmail.com>
2024-06-05 06:58:39 -0500

    create entities

---
+++ PruebaDesempeno/src/main/java/com/riwi/PruebaDesempeno/domain/entities/LessonEntity.java
@@ -0,0 +1,52 @@
+package com.riwi.PruebaDesempeno.domain.entities;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "lesson")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LessonEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    @Lob
+    private String content;
+    @Column(nullable = false)
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+    @Column(nullable = false)
+    private Boolean active;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "class_id", referencedColumnName = "id")
+    private ClassEntity classEntity;
+
+    @OneToMany(fetch = FetchType.EAGER,
+    cascade = CascadeType.ALL,
+    mappedBy = "lessonEntity",
+    orphanRemoval = false)
+    private List<MultimediaEntity> multimediaEntity;
+}
